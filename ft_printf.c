f0eca6d636713e01749c3ad8193eb089e573e11b
lkilpela <lkilpela@student.hive.fi>
2023-12-05 12:21:31 +0200

    updated

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,32 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 12:58:32 by lkilpela          #+#    #+#             */
-/*   Updated: 2023/12/05 11:49:01 by lkilpela         ###   ########.fr       */
+/*   Updated: 2023/12/05 12:20:24 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_parse(const char *format, int *i, va_list ap)
+int	ft_parse(const char format, va_list ap)
 {
 	int	count;
 
 	count = 0;
-	if (format[*i] == 'c')
+	if (format == 'c')
 		count = ft_putchar(va_arg(ap, int));
-	else if (format[*i] == 's')
+	else if (format == 's')
 		count = ft_putstr(va_arg(ap, char *));
-	else if (format[*i] == 'd' || format[*i] == 'i')
+	else if (format == 'd' || format == 'i')
 		count = ft_putnbr(va_arg(ap, int));
-	else if (format[*i] == 'u')
+	else if (format == 'u')
 		count += ft_putunbr(va_arg(ap, unsigned int));
-	else if (format[*i] == 'x')
+	else if (format == 'x')
 		count = ft_putnbr_hex(va_arg(ap, unsigned int), 0);
-	else if (format[*i] == 'X')
+	else if (format == 'X')
 		count = ft_putnbr_hex(va_arg(ap, unsigned int), 1);
-	else if (format[*i] == 'p')
+	else if (format == 'p')
 		count = ft_putptr(va_arg(ap, void *));
-	else if (format[*i] == '%')
+	else if (format == '%')
 		count = ft_putchar('%');
 	return (count);
 }
@@ -39,20 +39,27 @@ int	ft_parse(const char *format, int *i, va_list ap)
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	int		i;
 	int		count;
 	int		temp;
 
-	i = 0;
 	count = 0;
 	va_start(ap, format);
-	while (format[i])
+	while (*format)
 	{
-		 if (format[i] == '%' && (temp = ft_parse(format, &++i, ap)) != -1)
+		if (*format == '%')
+		{
+			temp = ft_parse(*(++format), ap);
+			if (count == -1)
+				return (-1);
 			count += temp;
-		else if (format[i] != '%' && ft_putchar(format[i++]) != -1)
+		}
+		else
+		{
+			if (ft_putchar(*format) == -1)
+				return (-1);
 			count++;
-		i++;
+		}
+		format++;
 	}
 	va_end(ap);
 	return (count);
